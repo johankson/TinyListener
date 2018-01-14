04c77e4bb577b0d129aa721e5cec7b43ee934f38
Johan Karlsson <johan.kson@gmail.com>
2018-01-14 12:10:07 +0100

    Fixed net standard client

---
+++ TinyListener.Client.Tests/ClientTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TinyListener.Client.Tests
+{
+    public class ClientTest
+    {
+        [Fact]
+        public async Task SimpleSendTest()
+        {
+            // 
+            var client = new TinyListener();
+            await client.Send("test-channel", "Ankor är söta");
+        }
+    }
+}
---
+++ TinyListener.Client/TinyListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TinyListener.Client
+{
+    public class TinyListener
+    {
+        private readonly HttpClient _client;
+
+        public TinyListener()
+        {
+            // TODO Add injection of HttpClient or a factory?
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://tinylistener.azurewebsites.net");
+        }
+
+        public async Task Send(string channel, string data)
+        {
+            // TODO Json serialization
+            var json = "{\"data\":\"" + data + "\"}";
+
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var result = await _client.PostAsync($"/api/listener/{channel}", content);
+        }
+    }
+}
